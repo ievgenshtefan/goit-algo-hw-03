b94ce4b8d48344f13cab55139237d8012964f059
ievgenshtefan <yevgenchik@gmail.com>
2024-10-01 15:34:01 +0300

    ver.2

--- hw03_02.py
+++ hw03_02.py
@@ -2,7 +2,7 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     random_numbers = set()
-    if min >= 1 and max <= 1000:
+    if min >= 1 and max <= 1000 and max > min and (max - min) >= quantity:
         for count in range(1, quantity+1):
             random_numbers.add(random.randint(min, max))
         random_numbers = list(random_numbers)
@@ -13,5 +13,5 @@ def get_numbers_ticket(min, max, quantity):
         return random_numbers
 
 
-lottery_numbers = get_numbers_ticket(1, 49, 16)
+lottery_numbers = get_numbers_ticket(5, 36, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
