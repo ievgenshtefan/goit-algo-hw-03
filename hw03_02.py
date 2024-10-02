31e595f3543dac775c327326c182fc314b14c9fb
ievgenshtefan <ievgenshtefan@users.noreply.github.com>
2024-10-02 22:49:40 +0300

    ver.5

--- hw03_02.py
+++ hw03_02.py
@@ -2,21 +2,16 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     random_numbers = set()
-    empty_numbers = set()
-    if min >= 1 and max <= 1000 and max > min and (max - min) >= quantity:
-        for count in range(1, quantity+1):
-            random_numbers.add(random.randint(min, max))
+    if (min >= 1) and (max <= 1000) and (max > min) and (max - min) >= quantity:
+        while len(random_numbers) != quantity:
+            random_numbers.add(random.randrange(min, max))
         random_numbers = list(random_numbers)
         random_numbers.sort()
-        if len(random_numbers) == quantity:
-            return random_numbers
-        else:
-            empty_numbers = list(empty_numbers)
-            return empty_numbers
+        return random_numbers
     else:
-        empty_numbers = list(empty_numbers)
-        return empty_numbers
+        random_numbers = list(random_numbers)
+        return random_numbers
 
 
-lottery_numbers = get_numbers_ticket(5, 36, 5)
+lottery_numbers = get_numbers_ticket(10, 35, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
