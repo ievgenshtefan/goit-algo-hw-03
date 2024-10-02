2ecc46e1232ae630d01aa6149cf5ce08efa155da
ievgenshtefan <yevgenchik@gmail.com>
2024-10-02 15:02:45 +0300

    ver.4

--- hw03_02.py
+++ hw03_02.py
@@ -2,6 +2,7 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     random_numbers = set()
+    empty_numbers = set()
     if min >= 1 and max <= 1000 and max > min and (max - min) >= quantity:
         for count in range(1, quantity+1):
             random_numbers.add(random.randint(min, max))
@@ -9,9 +10,12 @@ def get_numbers_ticket(min, max, quantity):
         random_numbers.sort()
         if len(random_numbers) == quantity:
             return random_numbers
+        else:
+            empty_numbers = list(empty_numbers)
+            return empty_numbers
     else:
-        random_numbers = list(random_numbers)
-        return random_numbers
+        empty_numbers = list(empty_numbers)
+        return empty_numbers
 
 
 lottery_numbers = get_numbers_ticket(5, 36, 5)
