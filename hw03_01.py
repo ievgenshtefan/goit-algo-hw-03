f181a08510bf08eff6346d468c93354f3d56f464
ievgenshtefan <yevgenchik@gmail.com>
2024-10-02 14:27:21 +0300

    ver.3

--- hw03_01.py
+++ hw03_01.py
@@ -9,8 +9,8 @@ def get_days_from_today(date):
         print(f"Format of the date is not correct. Must be YYYY-MM-DD.")
     else:
         today_day = datetime.today().date()
-        diff_day = abs(today_day - user_date)
-    return diff_day.days
+        diff_day = today_day - user_date
+        return diff_day.days
 
 request = get_days_from_today(test_day)
 print(request)
